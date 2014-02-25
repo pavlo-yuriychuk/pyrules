8710f856d71678c170df3d64aeca416c5aae966c
Pavlo Iuriichuk <pavlo.iuriichuk@globallogic.com>
2014-02-25 18:40:39 +0200

    Initial commit

---
+++ financial/__init__.py
@@ -0,0 +1,32 @@
+class MarketingRule:
+	def __init__(self):
+		pass
+
+	def apply(self, total, checque, prices_list):
+		return cheque
+
+class Checque:
+	def __init__(self, items = ""):
+		self.items = self.from_string(items)
+
+	def from_string(self, value):
+		return value.split(" ")
+
+class CashDesk:
+	def __init__(self, rules, prices_list):
+		self.rules = rules
+		self.prices_list = dict(prices_list)
+
+	def calculate(self, cheque):
+		total = reduce(lambda acc, x: acc + y.price, cheque.items, 0)
+		for rule in self.rules:
+			total = rule.apply(total, cheque, self.prices_list)
+		return total
+
+DEFAULT_RULES = []
+DEFAULT_PRICES_LIST = [("FR", 3.11), ("CF", 11.23), ("SR", 5.00)]
+
+if __name__ == "__main__":
+	assert CashDesk(DEFAULT_RULES, DEFAULT_PRICES_LIST).calculate(Checque("FR FR")) == 3.11
+	assert CashDesk(DEFAULT_RULES, DEFAULT_PRICES_LIST).calculate(Checque("SR SR FR SR")) == 16.61
+	assert CashDesk(DEFAULT_RULES, DEFAULT_PRICES_LIST).calculate(Checque("FR SR FR FR CF")) == 22.45
